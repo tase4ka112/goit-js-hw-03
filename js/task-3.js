e163b79d4196ea5b9d3a87f31d89ee579f6a0fc0
Tase4ka112 <tase4kakarpenko@gmail.com>
2024-03-05 20:42:38 +0200

    hw-3

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(number, value) {
+  const newArray = [];
+  for (let index = 0; index < number.length; index++) {
+    if (number[index] > value) {
+      newArray.push(number[index]);
+    }
+  }
+  return newArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
